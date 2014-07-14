e7b3b41e6d47bae6e108406d312c42bf64fa9134
Oliver Wickham <mr.o.wickham@gmail.com>
2014-07-14 14:19:52 +0100

    Fix subtle bug which fails tests dependant the order they are executed, due to some global state

--- test/controllers/ApplicationUnitSpec.scala
+++ test/controllers/ApplicationUnitSpec.scala
@@ -1,8 +1,8 @@
 package controllers
 
-import helpers.{UnitSpec, WithApplication}
+import helpers.{UnitSpec}
 import org.mockito.Mockito.when
-import play.api.test.{FakeApplication, FakeRequest}
+import play.api.test.{WithApplication, FakeApplication, FakeRequest}
 import play.api.test.Helpers._
 import utils.helpers.Config
 
@@ -22,7 +22,7 @@ final class ApplicationUnitSpec extends UnitSpec {
     }
 
     "redirect the user to the start url when there is an application context set " in
-      new WithApplication(FakeApplication(additionalConfiguration = Map("application.context" -> "/testContext/"))) {
+      new WithApplication(app = FakeApplication(additionalConfiguration = Map("application.context" -> "/testContext/"))) {
         implicit val config = configWithStartUrl("/testStart")
         val result = new ApplicationRoot().index(FakeRequest())
         redirectLocation(result) should equal(Some("/testContext/testStart"))
