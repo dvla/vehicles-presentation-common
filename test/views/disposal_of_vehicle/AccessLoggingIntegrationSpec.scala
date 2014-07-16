3cb72c250b20acc3a25e1c13e52f517c2fe5807f
Scott Walker <scott.walker@valtech.co.uk>
2014-07-16 11:43:46 +0100

    Refactor: Tidy up code

--- test/views/disposal_of_vehicle/AccessLoggingIntegrationSpec.scala
+++ test/views/disposal_of_vehicle/AccessLoggingIntegrationSpec.scala
@@ -16,14 +16,15 @@ import pages.disposal_of_vehicle.{BeforeYouStartPage, BusinessChooseYourAddressP
 import play.api.LoggerLike
 import play.api.test.FakeApplication
 
-class AccessLoggingIntegrationSpec extends UiSpec with TestHarness with MockitoSugar  with WebBrowserDSL {
+class AccessLoggingIntegrationSpec extends UiSpec with TestHarness with MockitoSugar with WebBrowserDSL {
 
   "Access Logging" should {
+    
     "Log access that complete with success" in new WebBrowser(testApp1) {
       go to BeforeYouStartPage
 
       val infoLogs = mockLoggerTest1.captureLogInfos(1)
-      infoLogs.get(0) should include("""] "GET /sell-to-the-trade/before-you-start HTTP/1.1" 200""")
+      infoLogs.get(0) should include( """] "GET /sell-to-the-trade/before-you-start HTTP/1.1" 200""")
     }
 
     "Log access that are completed because of Exception" in new WebBrowser(testApp2) {
@@ -33,8 +34,8 @@ class AccessLoggingIntegrationSpec extends UiSpec with TestHarness with MockitoS
       httpResponse.close()
 
       val infoLogs = mockLoggerTest2.captureLogInfos(2)
-      infoLogs.get(0) should include("""] "POST /sell-to-the-trade/business-choose-your-address HTTP/1.1" 303""")
-      infoLogs.get(1) should include("""] "GET /sell-to-the-trade/error/""")
+      infoLogs.get(0) should include( """] "POST /sell-to-the-trade/business-choose-your-address HTTP/1.1" 303""")
+      infoLogs.get(1) should include( """] "GET /sell-to-the-trade/error/""")
     }
 
     "Log access to unknown urls" in new WebBrowser(testApp3) {
@@ -45,8 +46,8 @@ class AccessLoggingIntegrationSpec extends UiSpec with TestHarness with MockitoS
 
       val infoLogs = mockLoggerTest3.captureLogInfos(2)
 
-      infoLogs.get(0) should include("""] "POST /some/unknown/url HTTP/1.1" 303""")
-      infoLogs.get(1) should include("""] "GET /sell-to-the-trade/error/""")
+      infoLogs.get(0) should include( """] "POST /some/unknown/url HTTP/1.1" 303""")
+      infoLogs.get(1) should include( """] "GET /sell-to-the-trade/error/""")
     }
 
     "not log any access for the healthcheck url" in new WebBrowser(testApp4) {
@@ -87,15 +88,26 @@ class AccessLoggingIntegrationSpec extends UiSpec with TestHarness with MockitoS
   }
 
   private val mockLoggerTest1 = new MockLogger
+
   private def testApp1 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest1)))
+
   private val mockLoggerTest2 = new MockLogger
+
   private def testApp2 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest2)))
+
   private val mockLoggerTest3 = new MockLogger
+
   private def testApp3 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest3)))
+
   private val mockLoggerTest4 = new MockLogger
+
   private def testApp4 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest4)))
+
   private val mockLoggerTest5 = new MockLogger
+
   private def testApp5 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest5)))
+
   private val mockLoggerTest6 = new MockLogger
+
   private def testApp6 = FakeApplication(withGlobal = Some(new TestGlobalWithMockLogger(mockLoggerTest6)))
 }
