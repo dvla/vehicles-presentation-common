96b9461f985f8c3a9ff03f3137f61f0af303c2ec
Lawrence Wagerfield <lawrence@dmz.wagerfield.com>
2014-07-29 13:23:58 +0100

    Removed more redundant form Ids

--- app/mappings/disposal_of_vehicle/Help.scala
+++ app/mappings/disposal_of_vehicle/Help.scala
@@ -1,5 +1,8 @@
 package mappings.disposal_of_vehicle
 
+/**
+ * The following are used to deduplicate code across views and tests; they are not used in view-model mappings.
+ */
 object Help {
   final val ExitId = "Exit"
   final val BackId = "Back"
--- app/mappings/disposal_of_vehicle/MicroserviceError.scala
+++ app/mappings/disposal_of_vehicle/MicroserviceError.scala
@@ -1,5 +1,8 @@
 package mappings.disposal_of_vehicle
 
+/**
+ * The following are used to deduplicate code across views and tests; they are not used in view-model mappings.
+ */
 object MicroserviceError {
   final val TryAgainId = "tryagain"
   final val ExitId = "exit"
