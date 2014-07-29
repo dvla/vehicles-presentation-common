9819f6fbfc132680853e13e6ddf0110db29a6e04
Lawrence Wagerfield <lawrence@dmz.wagerfield.com>
2014-07-29 12:50:18 +0100

    Moved DisposeFormViewModel mapping

--- app/viewmodels/DisposeFormViewModel.scala
+++ app/viewmodels/DisposeFormViewModel.scala
@@ -2,7 +2,18 @@ package viewmodels
 
 import models.DayMonthYear
 import models.domain.common.CacheKey
+import play.api.data.Mapping
 import play.api.libs.json.Json
+import services.DateService
+import constraints.common.DayMonthYear.{validDate, after, notInFuture}
+import mappings.common.Consent.consent
+import mappings.common.DayMonthYear.dayMonthYear
+import mappings.common.Mileage.mileage
+import mappings.disposal_of_vehicle.Dispose.MileageId
+import mappings.disposal_of_vehicle.Dispose.DateOfDisposalId
+import mappings.disposal_of_vehicle.Dispose.DateOfDisposalYearsIntoThePast
+import mappings.disposal_of_vehicle.Dispose.ConsentId
+import mappings.disposal_of_vehicle.Dispose.LossOfRegistrationConsentId
 
 final case class DisposeFormViewModel(mileage: Option[Int],
                                   dateOfDisposal: DayMonthYear,
@@ -18,4 +29,16 @@ object DisposeFormViewModel {
   final val DisposeFormTransactionIdCacheKey = "disposeFormTransactionId"
   final val DisposeFormTimestampIdCacheKey = "disposeFormTimestampId"
   final val DisposeFormRegistrationNumberCacheKey = "disposeFormRegistrationNumber"
+
+  object Form {
+    def mapping(dateService: DateService): Mapping[DisposeFormViewModel] =
+      play.api.data.Forms.mapping(
+        MileageId -> mileage(),
+        DateOfDisposalId -> dayMonthYear.verifying(validDate(),
+          after(earliest = (dateService.today - DateOfDisposalYearsIntoThePast).years),
+          notInFuture(dateService)),
+        ConsentId -> consent,
+        LossOfRegistrationConsentId -> consent
+      )(DisposeFormViewModel.apply)(DisposeFormViewModel.unapply)
+  }
 }
