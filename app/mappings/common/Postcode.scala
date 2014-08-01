fca21a22efdb4e6b30a71051702e79e4603ca8ce
Ian Stainer <ian.stainer@valtech.co.uk>
2014-08-01 15:25:04 +0100

    Moved the Postcode mapping into the common library. The non-mapping related code is in a new Postcode object in its original location

--- app/mappings/common/Postcode.scala
+++ app/mappings/common/Postcode.scala
@@ -1,17 +1,5 @@
 package mappings.common
 
-import uk.gov.dvla.vehicles.presentation.common.views.constraints.Postcode.validPostcode
-import play.api.data.Mapping
-import uk.gov.dvla.vehicles.presentation.common.views.helpers.FormExtensions
-import FormExtensions.nonEmptyTextWithTransform
-
 object Postcode {
   final val PostcodeId = "postcode"
-  final val Key = "postCode"
-  private final val MinLength = 5
-  final val MaxLength = 8
-
-  def postcode: Mapping[String] = {
-    nonEmptyTextWithTransform(_.toUpperCase.trim)(MinLength, MaxLength) verifying validPostcode
-  }
 }
