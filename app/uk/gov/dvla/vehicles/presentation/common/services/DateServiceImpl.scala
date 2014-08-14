b45d7bbdc870bbe40fc74694eb1cd6f25adcbad1
Scott Walker <scott.walker@valtech.co.uk>
2014-08-14 10:33:45 +0100

    US951: commonised DropDown and DateServiceImpl

---
+++ app/uk/gov/dvla/vehicles/presentation/common/services/DateServiceImpl.scala
@@ -0,0 +1,13 @@
+package uk.gov.dvla.vehicles.presentation.common.services
+
+import org.joda.time.{DateTime, Instant}
+import uk.gov.dvla.vehicles.presentation.common.views.models.DayMonthYear
+
+final class DateServiceImpl extends DateService {
+
+  override def today = DayMonthYear.today
+
+  override def now = Instant.now()
+
+  override def dateTimeISOChronology: String = DateTime.now().toString
+}
