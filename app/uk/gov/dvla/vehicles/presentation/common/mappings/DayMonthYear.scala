046b85c67932bb87451b4c66e9d1825aed77453d
Geraint Jones <geraint.jones@valtech.co.uk>
2014-08-22 13:41:22 +0100

    added the ability to obtain a time via the date service down to milliseconds

--- app/uk/gov/dvla/vehicles/presentation/common/mappings/DayMonthYear.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/mappings/DayMonthYear.scala
@@ -1,6 +1,6 @@
 package uk.gov.dvla.vehicles.presentation.common.mappings
 
-import play.api.data.Forms.{mapping, number, optional}
+import play.api.data.Forms._
 import play.api.data.Mapping
 import uk.gov.dvla.vehicles.presentation.common.views
 import views.constraints.DayMonthYear.required
@@ -12,16 +12,22 @@ object DayMonthYear {
   final val YearId = "year"
   final val HourId = "hour"
   final val MinutesId = "minutes"
+  final val SecondsId = "seconds"
+  final val MillisecondsId = "milliseconds"
   final val MaxDaysInMonth = 31
   final val MaxMonthsInYear = 12
   final val MaxHoursInDay = 24
   final val MaxMinutesInHour = 59
+  final val MaxSecondsInMinute = 59
+  final val MaxMillisecondsInSecond = 999
 
   val dayMonthYear: Mapping[uk.gov.dvla.vehicles.presentation.common.views.models.DayMonthYear] = mapping(
     DayId -> number(max = MaxDaysInMonth).verifying(required),
     MonthId -> number(max = MaxMonthsInYear).verifying(required),
     YearId -> number.verifying(required),
     HourId -> optional(number(min = 0, max = MaxHoursInDay)),
-    MinutesId -> optional(number(min = 0, max = MaxMinutesInHour))
+    MinutesId -> optional(number(min = 0, max = MaxMinutesInHour)),
+    SecondsId -> optional(number(min = 0, max = MaxSecondsInMinute)),
+    MillisecondsId -> optional(number(min = 0, max = MaxMillisecondsInSecond))
   )(models.DayMonthYear.apply)(models.DayMonthYear.unapply)
 }
