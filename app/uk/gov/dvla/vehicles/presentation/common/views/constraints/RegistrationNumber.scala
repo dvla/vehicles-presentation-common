f1fc5458ef7fca8a866959516ad901ba0a4c25ec
Oliver Wickham <mr.o.wickham@gmail.com>
2014-09-08 19:55:59 +0100

    Minor refactor to use built in pattern constraint and remove unnecessary comments

--- app/uk/gov/dvla/vehicles/presentation/common/views/constraints/RegistrationNumber.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/views/constraints/RegistrationNumber.scala
@@ -1,11 +1,12 @@
 package uk.gov.dvla.vehicles.presentation.common.views.constraints
 
-import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
+import play.api.data.validation.Constraint
+import play.api.data.validation.Constraints.pattern
 
 object RegistrationNumber {
 
-  def validRegistrationNumber: Constraint[String] = Constraint("constraint.restricted.validVrn") { input =>
-    val whitelist =
+  def validRegistrationNumber: Constraint[String] = pattern(
+    regex =
       """^
         |([A-Za-z]{3}[0-9]{1,4})|
         |([A-Za-z][0-9]{1,3}[A-Za-z]{3})|
@@ -14,11 +15,10 @@ object RegistrationNumber {
         |([A-Za-z]{1,3}[0-9]{1,3})|
         |([0-9]{1,4}[A-Za-z]{1,3})|
         |([A-Za-z]{1,2}[0-9]{1,4})
-        |$""".stripMargin.replace("\n", "").r
-
-    if (whitelist.pattern.matcher(input.replace(" ", "")).matches) Valid
-    else Invalid(ValidationError("error.restricted.validVrnOnly"))
-  }
+        |$""".stripMargin.replace("\n", "").r,
+    name = "constraint.restricted.validVrn",
+    error = "error.restricted.validVrnOnly"
+  )
 
   def formatVrm(vrm: String): String = {
     val FiveSpaceCharPadding = " " * 5
@@ -27,45 +27,45 @@ object RegistrationNumber {
     val TwoSpaceCharPadding = " " * 2
     val OneSpaceCharPadding = " " * 1
 
-    val AA99AAA = "([A-Z]{2}[0-9]{2}[A-Z]{3})".r // AA99AAA
-    val A9 = "([A-Z][0-9])".r // A9
-    val A99 = "([A-Z][0-9]{2})".r // A99
-    val A999 = "([A-Z][0-9]{3})".r // A999
-    val A9999 = "([A-Z][0-9]{4})".r // A9999
-    val AA9 = "([A-Z]{2}[0-9])".r // AA9
-    val AA99 = "([A-Z]{2}[0-9]{2})".r // AA99
-    val AA999 = "([A-Z]{2}[0-9]{3})".r // AA999
-    val AA9999 = "([A-Z]{2}[0-9]{4})".r // AA9999
-    val AAA9 = "([A-Z]{3}[0-9])".r // AAA9
-    val AAA99 = "([A-Z]{3}[0-9]{2})".r // AAA99
-    val AAA999 = "([A-Z]{3}[0-9]{3})".r // AAA999
-    val AAA9999 = "([A-Z]{3}[0-9]{4})".r // AAA9999
-    val AAA9Y = "([A-Z]{3}[0-9][A-Z])".r // AAA9Y
-    val AAA99Y = "([A-Z]{3}[0-9]{2}[A-Z])".r // AAA99Y
-    val AAA999Y = "([A-Z]{3}[0-9]{3}[A-Z])".r // AAA999Y
-    val `9A` = "([0-9][A-Z])".r // 9A
-    val `0009  A` = "([0]{3}[0-9][A-Z])".r // 0009  A
-    val `9AA` = "([0-9][A-Z]{2})".r // 9AA
-    val `0009 AA` = "([0]{3}[0-9][A-Z]{2})".r // 0009 AA
-    val `9AAA` = "([0-9][A-Z]{3})".r // 9AAA
-    val `0009AAA` = "([0]{3}[0-9][A-Z]{3})".r // 0009AAA
-    val `99A` = "([0-9]{2}[A-Z])".r // 99A
-    val `0099  A` = "([0]{2}[0-9]{2}[A-Z])".r // 0099  A
-    val `99AA` = "([0-9]{2}[A-Z]{2})".r // 99AA
-    val `0099 AA` = "([0]{2}[0-9]{2}[A-Z]{2})".r // 0099 AA
-    val `99AAA` = "([0-9]{2}[A-Z]{3})".r // 99AAA
-    val `0099AAA` = "([0]{2}[0-9]{2}[A-Z]{3})".r // 0099AAA
-    val `999A` = "([0-9]{3}[A-Z])".r // 999A
-    val `0999  A` = "([0][0-9]{3}[A-Z])".r // 0999  A
-    val `999AA` = "([0-9]{3}[A-Z]{2})".r // 999AA
-    val `0999 AA` = "([0][0-9]{3}[A-Z]{2})".r // 0999 AA
-    val `9999AAA` = "([0-9]{3}[A-Z]{3})".r // 9999AAA
-    val `0999AAA` = "([0][0-9]{3}[A-Z]{3})".r // 0999AAA
-    val `9999A` = "([0-9]{4}[A-Z])".r // 9999A
-    val `9999AA` = "([0-9]{4}[A-Z]{2})".r // 9999AA
-    val `A9AAA` = "([A-Z][0-9][A-Z]{3})".r // A9AAA
-    val `A99AAA` = "([A-Z][0-9]{2}[A-Z]{3})".r // A99AAA
-    val `A999AAA` = "([A-Z][0-9]{3}[A-Z]{3})".r // A999AAA
+    val AA99AAA = "([A-Z]{2}[0-9]{2}[A-Z]{3})".r
+    val A9 = "([A-Z][0-9])".r
+    val A99 = "([A-Z][0-9]{2})".r
+    val A999 = "([A-Z][0-9]{3})".r
+    val A9999 = "([A-Z][0-9]{4})".r
+    val AA9 = "([A-Z]{2}[0-9])".r
+    val AA99 = "([A-Z]{2}[0-9]{2})".r
+    val AA999 = "([A-Z]{2}[0-9]{3})".r
+    val AA9999 = "([A-Z]{2}[0-9]{4})".r
+    val AAA9 = "([A-Z]{3}[0-9])".r
+    val AAA99 = "([A-Z]{3}[0-9]{2})".r
+    val AAA999 = "([A-Z]{3}[0-9]{3})".r
+    val AAA9999 = "([A-Z]{3}[0-9]{4})".r
+    val AAA9Y = "([A-Z]{3}[0-9][A-Z])".r
+    val AAA99Y = "([A-Z]{3}[0-9]{2}[A-Z])".r
+    val AAA999Y = "([A-Z]{3}[0-9]{3}[A-Z])".r
+    val `9A` = "([0-9][A-Z])".r
+    val `0009  A` = "([0]{3}[0-9][A-Z])".r
+    val `9AA` = "([0-9][A-Z]{2})".r
+    val `0009 AA` = "([0]{3}[0-9][A-Z]{2})".r
+    val `9AAA` = "([0-9][A-Z]{3})".r
+    val `0009AAA` = "([0]{3}[0-9][A-Z]{3})".r
+    val `99A` = "([0-9]{2}[A-Z])".r
+    val `0099  A` = "([0]{2}[0-9]{2}[A-Z])".r
+    val `99AA` = "([0-9]{2}[A-Z]{2})".r
+    val `0099 AA` = "([0]{2}[0-9]{2}[A-Z]{2})".r
+    val `99AAA` = "([0-9]{2}[A-Z]{3})".r
+    val `0099AAA` = "([0]{2}[0-9]{2}[A-Z]{3})".r
+    val `999A` = "([0-9]{3}[A-Z])".r
+    val `0999  A` = "([0][0-9]{3}[A-Z])".r
+    val `999AA` = "([0-9]{3}[A-Z]{2})".r
+    val `0999 AA` = "([0][0-9]{3}[A-Z]{2})".r
+    val `9999AAA` = "([0-9]{3}[A-Z]{3})".r
+    val `0999AAA` = "([0][0-9]{3}[A-Z]{3})".r
+    val `9999A` = "([0-9]{4}[A-Z])".r
+    val `9999AA` = "([0-9]{4}[A-Z]{2})".r
+    val `A9AAA` = "([A-Z][0-9][A-Z]{3})".r
+    val `A99AAA` = "([A-Z][0-9]{2}[A-Z]{3})".r
+    val `A999AAA` = "([A-Z][0-9]{3}[A-Z]{3})".r
 
     vrm.toUpperCase.replace(OneSpaceCharPadding, "") match {
       case AA99AAA(v) => v.substring(0, 4) + OneSpaceCharPadding + v.substring(4, 7)
