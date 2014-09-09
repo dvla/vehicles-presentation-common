43464343c1ddfd9474c96d036a5ed956b7409821
Oliver Wickham <mr.o.wickham@gmail.com>
2014-09-09 13:48:26 +0100

    Uppercase the VRM and remove spaces in the mapping

--- app/uk/gov/dvla/vehicles/presentation/common/mappings/VehicleRegistrationNumber.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/mappings/VehicleRegistrationNumber.scala
@@ -1,12 +1,16 @@
 package uk.gov.dvla.vehicles.presentation.common.mappings
 
-import play.api.data.Forms.nonEmptyText
 import play.api.data.Mapping
+import uk.gov.dvla.vehicles.presentation.common.views
+import views.helpers.FormExtensions.nonEmptyTextWithTransform
 import uk.gov.dvla.vehicles.presentation.common.views.constraints.RegistrationNumber.validRegistrationNumber
 
 object VehicleRegistrationNumber {
   final val MinLength = 2
   final val MaxLength = 8
 
-  def registrationNumber: Mapping[String] = nonEmptyText(MinLength, MaxLength) verifying validRegistrationNumber
+  def registrationNumber: Mapping[String] = {
+    def transform(s: String) = s.replace(" ", "").toUpperCase
+    nonEmptyTextWithTransform(transform)(MinLength, MaxLength) verifying validRegistrationNumber
+  }
 }
