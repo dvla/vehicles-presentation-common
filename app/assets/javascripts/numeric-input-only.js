656bc5e9e54f83e79cb27728a5a391220e1919fe
Ian Stainer <ian.stainer@valtech.co.uk>
2014-09-10 12:45:27 +0100

    Added fleet number support to the valtechInputText widget

--- app/assets/javascripts/numeric-input-only.js
+++ app/assets/javascripts/numeric-input-only.js
@@ -12,3 +12,30 @@ function isNumber(event) {
         }
     return true;
 }
+
+function isFleetNumber(event) {
+    var backspace = 8;
+    var tab = 9;
+    var end = 35;
+    var home = 36;
+    var leftArrow = 37;
+    var downArrow = 40;
+    var deleteCode = 46;
+    var zero = 48;
+    var nine = 57;
+    var numpad0 = 96;
+    var numpad9 = 105;
+    var hyphen = 173;
+    if (event) {
+        var charCode = (event.which) ? event.which : event.keyCode;
+        console.log(charCode)
+        if (charCode != hyphen && charCode != tab && charCode != end &&
+            charCode != home && charCode != backspace && charCode != deleteCode &&
+            (charCode < zero || charCode > nine) &&
+            (charCode < numpad0 || charCode > numpad9) &&
+            (charCode < leftArrow || charCode > downArrow)
+           )
+           return false;
+        }
+    return true;
+}
