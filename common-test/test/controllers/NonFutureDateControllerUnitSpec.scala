a59a88fa788196803bf427155a653c45759cc8f5
Matthew Snaith <fault_line@hotmail.co.uk>
2014-09-22 10:58:14 +0100

    NonFutureDate tests added

---
+++ common-test/test/controllers/NonFutureDateControllerUnitSpec.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import helpers.{UnitSpec, WithApplication}
+import models.NonFutureDateModel
+import models.NonFutureDateModel.Key
+import org.joda.time.LocalDate
+import play.api.libs.json.{Json, Writes}
+import play.api.mvc.Cookie
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{OK, contentAsString, defaultAwaitTimeout}
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClearTextClientSideSession
+
+
+class NonFutureDateControllerUnitSpec extends UnitSpec {
+  private val nonFutureDateController = {
+    injector.getInstance(classOf[NonFutureDateController])
+  }
+
+  "present" should {
+    "show an empty form when no cookie are found" in new WithApplication {
+      val request = FakeRequest()
+      whenReady(nonFutureDateController.present(request)) {
+        r => r.header.status should equal(OK)
+      }
+    }
+
+    "show a full form when cookie is found" in new WithApplication {
+      val value = NonFutureDateModel(new LocalDate(1234, 12, 24))
+
+      val request = FakeRequest()
+        .withCookies(createCookie(Key.value, value))
+      val html = contentAsString(nonFutureDateController.present(request))
+      html should include("24")
+      html should include("12")
+      html should include("1234")
+    }
+  }
+
+  private def createCookie[A](key: String, value: A)(implicit tjs: Writes[A]): Cookie = {
+    import uk.gov.dvla.vehicles.presentation.common.clientsidesession.NoCookieFlags
+    val session = new ClearTextClientSideSession("trackingId")(new NoCookieFlags)
+    val json = Json.toJson(value).toString()
+    val cookieName = session.nameCookie(key)
+    session.newCookie(cookieName, json)
+  }
+}
---
+++ common-test/test/views/NonFutureDateIntegrationSpec.scala
@@ -0,0 +1,86 @@
+package views
+
+import com.github.nscala_time.time.Imports.LocalDate
+import helpers.UiSpec
+import helpers.webbrowser.TestHarness
+import org.joda.time.chrono.ISOChronology
+import pages.{NonFutureDatePage, ErrorPanel}
+import play.api.i18n.Messages
+
+class NonFutureDateIntegrationSpec extends UiSpec with TestHarness {
+
+  "Non future date" should {
+    "be on a page with the correct title" in new WebBrowser {
+      go to NonFutureDatePage.instance
+      page.title should equal(NonFutureDatePage.instance.title)
+    }
+
+    "not allow no values to be input" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("", "", "")
+      page.title should equal(NonFutureDatePage.instance.title)
+    }
+
+    "display appropriate error when day is missing" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("", "10", "1920")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display appropriate error when month is missing" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("10", "", "1920")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display appropriate error when year is missing" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("12", "10", "")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display appropriate error when day contains letters" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("a", "10", "1920")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display appropriate error when month contains letters" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("10", "a", "1920")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display appropriate error when year contains letters" in new WebBrowser {
+      NonFutureDatePage.instance.navigate("12", "10", "a")
+      ErrorPanel.text should include(Messages("error.nonFutureDate.invalid"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+
+    "validate the whole date is not in the future" in new WebBrowser {
+      val chronology = ISOChronology.getInstance()
+      val now = System.currentTimeMillis()
+      val day = chronology.dayOfMonth().get(now)
+      val month = chronology.monthOfYear().get(now)
+      val year = chronology.year().get(now)
+
+      NonFutureDatePage.instance.navigate((day + 1).toString, month.toString, year.toString)
+      ErrorPanel.text should include(Messages("error.nonFutureDate.inTheFuture"))
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "Pass a valid date - today" in new WebBrowser {
+      val today = LocalDate.today
+      NonFutureDatePage.instance.navigate(today.getDayOfMonth.toString, today.getMonthOfYear.toString, today.getYear.toString)
+
+      page.title should equal("Success")
+    }
+
+    "Pass a valid date - in the past" in new WebBrowser {
+      val today = LocalDate.today
+      NonFutureDatePage.instance.navigate("15", "10", "1980")
+
+      page.title should equal("Success")
+    }
+  }
+}
