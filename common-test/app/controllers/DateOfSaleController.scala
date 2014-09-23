3d87e7bcdf4c14456fea6476b28f970250eb8790
Matthew Snaith <fault_line@hotmail.co.uk>
2014-09-23 09:07:08 +0100

    DateOfSale widget added to common-test

---
+++ common-test/app/controllers/DateOfSaleController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.data.Form
+import play.api.mvc.{Action, Controller}
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichForm
+
+class DateOfSaleController @Inject()(implicit clientSideSessionFactory: ClientSideSessionFactory)
+  extends Controller {
+
+  private[controllers] val form = Form(models.DateOfSaleModel.Form.Mapping)
+
+  def present = Action { implicit request =>
+    Ok(views.html.valtechDateOfSaleView(form.fill()))
+  }
+
+  def submit = Action {
+    implicit request => {
+      form.bindFromRequest.fold(
+        invalidForm => BadRequest(views.html.valtechDateOfSaleView(invalidForm)),
+        validForm => {
+          val msg = s"Success - you have correctly declared your intent to proceed"
+          Ok(views.html.success(msg))
+        }
+      )
+    }
+  }
+}
