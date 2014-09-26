611ccf6eae947a78bdd6d963aee8d4867402e087
Chris Rudd <chris.rudd@valtech.co.uk>
2014-09-26 12:58:32 +0100

    Extended driver number regex to allow lower case text.

--- app/uk/gov/dvla/vehicles/presentation/common/views/constraints/DriverNumber.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/views/constraints/DriverNumber.scala
@@ -7,7 +7,7 @@ import uk.gov.dvla.vehicles.presentation.common.mappings.DriverNumber.{MinLength
 
 object DriverNumber {
 
-  private val validDriverNumberRegex = "[A-Z]{1}[A-Z0-9]{4}\\d([05][1-9]|[16][012])(0[1-9]|[12][0-9]|3[01])\\d[A-Z]{1}[A-Z9]{1}[98765432XWVUTSRPNMLKJHGFEDCBA]{1}[A-Z]{2}"
+  private val validDriverNumberRegex = "[a-zA-Z]{1}[a-zA-Z0-9]{4}\\d([05][1-9]|[16][012])(0[1-9]|[12][0-9]|3[01])\\d[a-zA-Z]{1}[a-zA-Z9]{1}[98765432xwvutsrpnmlkjhgfedcbaXWVUTSRPNMLKJHGFEDCBA]{1}[a-zA-Z]{2}"
 
   val ptr = Pattern.compile(validDriverNumberRegex)
 
