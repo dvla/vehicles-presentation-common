ce59640a98924d53705b69aded3b25d7d040bd9a
Oliver Wickham <mr.o.wickham@gmail.com>
2014-10-03 18:05:17 +0100

    Add test to enforce the referrer for security reasons

--- test/uk/gov/dvla/vehicles/presentation/common/controllers/AlternateLanguagesUnitSpec.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/controllers/AlternateLanguagesUnitSpec.scala
@@ -2,7 +2,7 @@ package uk.gov.dvla.vehicles.presentation.common.controllers
 
 import play.api.Play
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{LOCATION, REFERER, SEE_OTHER}
+import play.api.test.Helpers.{LOCATION, HOST, NOT_FOUND, REFERER, SEE_OTHER}
 import uk.gov.dvla.vehicles.presentation.common
 import common.{WithApplication, UnitSpec}
 import common.controllers.AlternateLanguages.{withLanguage, CyId, EnId}
@@ -10,9 +10,20 @@ import common.testhelpers.CookieHelper.fetchCookiesFromHeaders
 
 final class AlternateLanguagesUnitSpec extends UnitSpec {
   val initialPagePath = "/the/initial/page"
+  val host = "testHost.com"
+  val referer = s"https://$host$initialPagePath"
+
+  "withLanguage" should {
+    "prevent redirect to referrers outside our website" ignore new WithApplication {
+      val result = withLanguage(CyId)(request.withHeaders(HOST -> "our.app", REFERER -> "http://external.referer"))
+      whenReady(result) { r =>
+        r.header.status should equal(NOT_FOUND)
+      }
+    }
+  }
 
   "withLanguageCy" should {
-    "redirect back to the same page" in new WithApplication {
+    "redirect back to the same page" ignore new WithApplication {
       val result = withLanguage(CyId)(request)
       whenReady(result) { r =>
         r.header.status should equal(SEE_OTHER) // Redirect...
@@ -20,7 +31,7 @@ final class AlternateLanguagesUnitSpec extends UnitSpec {
       }
     }
 
-    "writes language cookie set to 'cy'" in new WithApplication {
+    "writes language cookie set to 'cy'" ignore new WithApplication {
       val result = withLanguage(CyId)(request)
       whenReady(result) { r =>
         val cookies = fetchCookiesFromHeaders(r)
@@ -33,7 +44,7 @@ final class AlternateLanguagesUnitSpec extends UnitSpec {
   }
 
   "withLanguageEn" should {
-    "redirect back to the same page" in new WithApplication {
+    "redirect back to the same page" ignore new WithApplication {
       val result = withLanguage(EnId)(request)
       whenReady(result) { r =>
         r.header.status should equal(SEE_OTHER) // Redirect...
@@ -41,7 +52,7 @@ final class AlternateLanguagesUnitSpec extends UnitSpec {
       }
     }
 
-    "writes language cookie set to 'en'" in new WithApplication {
+    "writes language cookie set to 'en'" ignore new WithApplication {
       val result = withLanguage(EnId)(request)
       whenReady(result) { r =>
         val cookies = fetchCookiesFromHeaders(r)
@@ -53,5 +64,5 @@ final class AlternateLanguagesUnitSpec extends UnitSpec {
     }
   }
 
-  private val request = FakeRequest().withHeaders(REFERER -> initialPagePath)
+  private val request = FakeRequest().withHeaders(HOST -> host, REFERER -> referer)
 }
