82404f0874aecfc72965cc1dfebd19804ed7ed02
Oliver Wickham <mr.o.wickham@gmail.com>
2014-10-06 15:49:09 +0100

    Add logging for the referer issue

--- app/uk/gov/dvla/vehicles/presentation/common/controllers/AlternateLanguages.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/controllers/AlternateLanguages.scala
@@ -1,8 +1,9 @@
 package uk.gov.dvla.vehicles.presentation.common.controllers
 
-import play.api.Play.current
 import play.api.i18n.Lang
+import play.api.Logger
 import play.api.mvc.{Action, Controller}
+import play.api.Play.current
 
 object AlternateLanguages extends Controller {
   final val CyId = "cy"
@@ -13,6 +14,8 @@ object AlternateLanguages extends Controller {
   def withLanguage(chosenLanguage: String) = Action { implicit request =>
     val refererOpt = request.headers.get(REFERER)
     val safeReferer = refererOpt.filter { _.stripPrefix("https://").stripPrefix("http://") startsWith request.host }
+
+    Logger.debug(s"referrer $refererOpt host ${request.host}")
 
     safeReferer.map { ref =>
       Redirect(ref).withLang(Lang(chosenLanguage))
