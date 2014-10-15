204fc95180349f41f56212a7f9490a5a3c144b92
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-10-15 17:02:56 +0100

    EnsureServiceOpenFilter html is now Appendable

--- app/uk/gov/dvla/vehicles/presentation/common/filters/EnsureServiceOpenFilter.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/filters/EnsureServiceOpenFilter.scala
@@ -1,17 +1,18 @@
 package uk.gov.dvla.vehicles.presentation.common.filters
 
 import org.joda.time.{DateTime, DateTimeZone}
-import play.api.mvc.{Result, Filter, RequestHeader,Results}
+import play.api.mvc.{Filter, RequestHeader, Result, Results}
+import play.twirl.api.HtmlFormat
 import uk.gov.dvla.vehicles.presentation.common.filters.ServiceOpen.whitelist
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 trait EnsureServiceOpenFilter extends Filter {
 
   protected val opening: Int
   protected val closing: Int
   protected val dateTimeZone: DateTimeZoneService
-  protected val html: Appendable
+  protected val html: HtmlFormat.Appendable
 
   override def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
     if (whitelist.exists(requestHeader.path.contains)) nextFilter(requestHeader)
