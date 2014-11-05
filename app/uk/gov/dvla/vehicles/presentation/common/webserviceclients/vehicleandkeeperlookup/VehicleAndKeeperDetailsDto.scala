12ea99c14495e51c609ed4260927f573a75aa0ad
Chris Rudd <chris.rudd@valtech.co.uk>
2014-11-05 13:28:30 +0000

    Reverted keeperEndDate as it is no longer a requirement.

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
@@ -1,6 +1,5 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup
 
-import org.joda.time.DateTime
 import play.api.libs.json.Json
 
 final case class VehicleAndKeeperDetailsDto(registrationNumber: String,
@@ -14,25 +13,9 @@ final case class VehicleAndKeeperDetailsDto(registrationNumber: String,
                                             keeperAddressLine3: Option[String],
                                             keeperAddressLine4: Option[String],
                                             keeperPostTown: Option[String],
-                                            keeperPostcode: Option[String],
-                                            keeperEndDate: Option[DateTime])
+                                            keeperPostcode: Option[String])
 
 object VehicleAndKeeperDetailsDto {
-
-  // TODO : I Suspect that we'll need this mapping for dates, but add it when testing demonstrates the need
-  // Handles this type of formatted string 2014-03-04T00:00:00.000Z
-//  implicit object DateTimeJsonFormat extends JsonFormat[DateTime] {
-//    def write(dateTime: DateTime) = {
-//      val formatter = ISODateTimeFormat.dateTime
-//      JsString(formatter.print(dateTime))
-//    }
-//
-//    def read(value: JsValue) = value match {
-//      case JsString(isoString) =>
-//        ISODateTimeFormat.dateTime().parseDateTime(isoString)
-//      case _ => deserializationError("Could not deserialize the iso date time")
-//    }
-//  }
 
   implicit val JsonFormat = Json.format[VehicleAndKeeperDetailsDto]
 }
