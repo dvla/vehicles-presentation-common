831ae6e44c9285a27481dfbeffc224f3b2589704
Matthew Snaith <fault_line@hotmail.co.uk>
2014-11-12 13:14:40 +0000

    us1656 driver number to be capitalised to enable acceptance tests to pass

--- app/uk/gov/dvla/vehicles/presentation/common/mappings/DriverNumber.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/mappings/DriverNumber.scala
@@ -1,14 +1,13 @@
 package uk.gov.dvla.vehicles.presentation.common.mappings
 
-import play.api.data.format.Formats.stringFormat
-import play.api.data.Forms.of
 import play.api.data.Mapping
 import uk.gov.dvla.vehicles.presentation.common.views.constraints.DriverNumber.validDriverNumber
+import uk.gov.dvla.vehicles.presentation.common.views.helpers.FormExtensions.nonEmptyTextWithTransform
 
 object DriverNumber {
   final val MinLength = 1
   final val MaxLength = 16
 
-  def driverNumber: Mapping[String] = of[String] verifying validDriverNumber
-
+  def driverNumber: Mapping[String] =
+    nonEmptyTextWithTransform(_.toUpperCase)(MinLength, MaxLength) verifying validDriverNumber
 }
