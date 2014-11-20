5d240c3e2c9cf971842f9007ebe866c54857f79b
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-11-20 16:43:07 +0000

    Remove a line that might have broke the build

--- common-test/test/uk/gov/dvla/vehicles/presentation/common/views/VersionIntegrationSpec.scala
+++ common-test/test/uk/gov/dvla/vehicles/presentation/common/views/VersionIntegrationSpec.scala
@@ -14,7 +14,7 @@ class VersionIntegrationSpec extends UiSpec with TestHarness {
       go.to(VersionPage)
       val t = fromInputStream(getClass.getResourceAsStream("/build-details.txt")).getLines().toList
 
-      info(s"""The class path build-details.txt "$t" should be contained in the verstion string "${page.source}"""")
+//      info(s"""The class path build-details.txt "$t" should be contained in the verstion string "${page.source}"""")
       t.filterNot(page.source.lines.contains(_)) should be(empty)
     }
   }
