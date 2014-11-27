3ca51185a4d7c45a8adb384260215610b1b075af
Scott Walker <scott.walker@valtech.co.uk>
2014-11-27 10:18:58 +0000

    US1580: we need to be able to simulate vrm locked for cucumber tests

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/bruteforceprevention/BruteForcePreventionServiceImpl.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/bruteforceprevention/BruteForcePreventionServiceImpl.scala
@@ -1,20 +1,23 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.bruteforceprevention
 
 import javax.inject.Inject
+
 import play.api.Logger
 import play.api.libs.json.Json
 import uk.gov.dvla.vehicles.presentation.common.model.BruteForcePreventionModel
 import uk.gov.dvla.vehicles.presentation.common.services.DateService
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 final class BruteForcePreventionServiceImpl @Inject()(config: BruteForcePreventionConfig,
                                                       ws: BruteForcePreventionWebService,
                                                       dateService: DateService) extends BruteForcePreventionService {
+
   private val maxAttempts: Int = config.maxAttemptsHeader.toInt
 
   override def isVrmLookupPermitted(vrm: String): Future[BruteForcePreventionModel] =
-    // Feature toggle until all developers have Redis and the brute force micro service setup locally.
+  // Feature toggle until all developers have Redis and the brute force micro service setup locally.
     if (config.isEnabled) {
       val returnedFuture = scala.concurrent.Promise[BruteForcePreventionModel]()
       ws.callBruteForce(vrm).map { resp =>
@@ -51,21 +54,43 @@ final class BruteForcePreventionServiceImpl @Inject()(config: BruteForcePreventi
       returnedFuture.future
     }
     else Future {
-      BruteForcePreventionModel.fromResponse(permitted = true,
-        BruteForcePreventionResponseDto(attempts = 0),
-        dateService,
-        maxAttempts = maxAttempts
-      )
+      // This code is only reached when brute force prevention is disabled, which should only be when we are running
+      // locally (i.e. sbt sandbox or sbt run) and NEVER in production.
+      val vrmAttempt2 = "ST05YYB"
+      val vrmLocked = "ST05YYC"
+      vrm match {
+        case `vrmAttempt2` =>
+          // Block access
+          BruteForcePreventionModel.fromResponse(permitted = false,
+            BruteForcePreventionResponseDto(attempts = maxAttempts),
+            dateService,
+            maxAttempts = maxAttempts
+          )
+        case `vrmLocked` =>
+          // Block access with warning
+          BruteForcePreventionModel.fromResponse(permitted = false,
+            BruteForcePreventionResponseDto(attempts = maxAttempts - 1),
+            dateService,
+            maxAttempts = maxAttempts
+          )
+        case _ =>
+          // Allow access
+          BruteForcePreventionModel.fromResponse(permitted = true,
+            BruteForcePreventionResponseDto(attempts = 0),
+            dateService,
+            maxAttempts = maxAttempts
+          )
+      }
     }
 
   override def reset(vrm: String): Future[Int] =
-    // Feature toggle until all developers have Redis and the brute force micro service setup locally.
+  // Feature toggle until all developers have Redis and the brute force micro service setup locally.
     if (config.isEnabled) {
       val returnedFuture = scala.concurrent.Promise[Int]()
       ws.reset(vrm).map { resp =>
         returnedFuture.success(resp.status)
       }.recover { case e: Throwable =>
-          returnedFuture.failure(e)
+        returnedFuture.failure(e)
       }
       returnedFuture.future
     }
