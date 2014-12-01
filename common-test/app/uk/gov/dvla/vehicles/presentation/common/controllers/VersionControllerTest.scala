d8d406b312f4b1853c5c8cea41be87cac29b5a24
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-12-01 18:39:41 +0000

    US1943 add some more integration tests tot he version controller

--- common-test/app/uk/gov/dvla/vehicles/presentation/common/controllers/VersionControllerTest.scala
+++ common-test/app/uk/gov/dvla/vehicles/presentation/common/controllers/VersionControllerTest.scala
@@ -1,4 +1,9 @@
 package uk.gov.dvla.vehicles.presentation.common.controllers
 
 // Used to test the version controller. Used in the routes file.
-class VersionControllerTest extends Version
+class VersionControllerTest extends Version(VersionControllerTest.missingUrl, VersionControllerTest.notParsingUrl)
+
+object VersionControllerTest {
+  val notParsingUrl = "not parsing url"
+  val missingUrl = "missing url"
+}
--- common-test/test/uk/gov/dvla/vehicles/presentation/common/views/VersionIntegrationSpec.scala
+++ common-test/test/uk/gov/dvla/vehicles/presentation/common/views/VersionIntegrationSpec.scala
@@ -1,5 +1,6 @@
 package uk.gov.dvla.vehicles.presentation.common.views
 
+import uk.gov.dvla.vehicles.presentation.common.controllers.VersionControllerTest
 import uk.gov.dvla.vehicles.presentation.common.helpers.UiSpec
 
 import uk.gov.dvla.vehicles.presentation.common.pages
@@ -13,8 +14,11 @@ class VersionIntegrationSpec extends UiSpec with TestHarness {
     "be declared and should include the build-details.txt from classpath" in new WebBrowser {
       go.to(VersionPage)
       val t = fromInputStream(getClass.getResourceAsStream("/build-details.txt")).getLines().toList
+      val pageSource = page.source
+      pageSource.lines.toSeq should contain allOf(t.head, t.tail.head, t.tail.tail.toSeq:_*)
 
-      page.source.lines.toSeq should contain allOf(t.head, t.tail.head, t.tail.tail.toSeq:_*)
+      pageSource should include(VersionControllerTest.missingUrl)
+      pageSource should include(VersionControllerTest.notParsingUrl)
     }
   }
 }
