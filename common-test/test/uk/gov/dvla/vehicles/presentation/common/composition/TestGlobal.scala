275ec66a07cd193b7ef46608fd9f20f92c52d10d
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-12-05 17:41:01 +0000

    US1838 make the TestGlobalCreator global method return an explicit type

--- common-test/test/uk/gov/dvla/vehicles/presentation/common/composition/TestGlobal.scala
+++ common-test/test/uk/gov/dvla/vehicles/presentation/common/composition/TestGlobal.scala
@@ -1,10 +1,10 @@
 package uk.gov.dvla.vehicles.presentation.common.composition
 
+import play.api.GlobalSettings
 import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.GlobalCreator
-
 
 object TestGlobal extends GlobalLike with TestComposition
 
 trait TestGlobalCreator extends GlobalCreator {
-  override def global = TestGlobal
+  override def global: GlobalSettings = TestGlobal
 }
