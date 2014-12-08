8f4eadd06d8fc45281fcf68234e0209dbcfe45e2
Makis Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2014-12-08 16:28:47 +0000

    US2001: Added method to retrieve a list from configuration.

--- app/uk/gov/dvla/vehicles/presentation/common/ConfigProperties.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/ConfigProperties.scala
@@ -4,6 +4,7 @@ import play.api.Play
 import scala.util.{Success, Try}
 
 object ConfigProperties {
+
   def getProperty(property: String, default: Int) =
     Try(Play.current.configuration.getInt(property).getOrElse(default)) match {
       case Success(s) => s
@@ -27,6 +28,14 @@ object ConfigProperties {
       case Success(s) => s
       case _ => default
     }
+  def getStringListProperty(property: String): Option[List[String]] = {
+    import collection.JavaConversions._ // configuration.getStringList returns a Java list but we need a scala list
+                                        // so import this here and convert the list bellow to a scala list
+    Try(Play.current.configuration.getStringList(property).map(_.toList)) match {
+      case Success(s) => s
+      case _ => None
+    }
+  }
 
   def getDurationProperty(property: String, default: Long) =
     Try(Play.current.configuration.getMilliseconds(property).getOrElse(default)) match {
