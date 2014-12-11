a96643a2d3e207edf56cead4c9fbcc8cab829bbe
Ian Stainer <ian.stainer@valtech.co.uk>
2014-12-11 10:37:13 +0000

    US1725 - moved new common test code class to vehicles-presentation-common test

--- test/uk/gov/dvla/vehicles/presentation/common/helpers/webbrowser/WebBrowserDriverWithJavaScript.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/helpers/webbrowser/WebBrowserDriverWithJavaScript.scala
@@ -2,4 +2,4 @@ package uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser
 
 import org.openqa.selenium.support.events.EventFiringWebDriver
 
-class WebBrowserDriverWithJavaScript extends EventFiringWebDriver(WebDriverFactory.webDriver("htmlunit", true))
+class WebBrowserDriverWithJavaScript extends EventFiringWebDriver(WebDriverFactory.webDriver("htmlunit", javascriptEnabled = true))
