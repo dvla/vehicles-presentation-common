f1bdb00b1938d53951761bb9ccfdb3ca184e31b9
Scott Walker <scott.walker@valtech.co.uk>
2014-12-16 16:00:08 +0000

    Added a firefox web browser driver as a type

--- test/uk/gov/dvla/vehicles/presentation/common/helpers/webbrowser/WebBrowserDriver.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/helpers/webbrowser/WebBrowserDriver.scala
@@ -1,6 +1,9 @@
 package uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser
 
 // Needed by the picocontainer
+
 import org.openqa.selenium.support.events.EventFiringWebDriver
 
-class WebBrowserDriver() extends EventFiringWebDriver(WebDriverFactory.webDriver)
+class WebBrowserDriver extends EventFiringWebDriver(WebDriverFactory.webDriver)
+
+class WebBrowserFirefoxDriver extends EventFiringWebDriver(WebDriverFactory.webDriver(targetBrowser = "firefox", javascriptEnabled = true))
