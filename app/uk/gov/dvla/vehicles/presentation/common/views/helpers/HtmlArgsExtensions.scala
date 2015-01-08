0c4065099b9dc139babb1a59343b13479f93d7c5
Scott Walker <scott.walker@valtech.co.uk>
2015-01-08 12:28:36 +0000

    HtmlArgsExtensions now uses the modern/simplified way of pimping that we use for CookieImplicits. Removed some of the test code that only applied when we had html5 validation

--- app/uk/gov/dvla/vehicles/presentation/common/views/helpers/HtmlArgsExtensions.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/views/helpers/HtmlArgsExtensions.scala
@@ -1,31 +1,24 @@
 package uk.gov.dvla.vehicles.presentation.common.views.helpers
 
-import play.api.Play
-import play.api.Play.current
 import scala.language.implicitConversions
 
 // See the Scala docs for value scala.language.implicitConversions for a discussion why the feature should be explicitly enabled.
 
 object HtmlArgsExtensions {
 
-  final class RichHtmlArgs(htmlArgs: Map[Symbol, Any], validationOff: Boolean = Play.isTest) {
-    private final val DefaultMaxLength = 60
+  implicit class RichHtmlArgs(val htmlArgs: Map[Symbol, Any]) extends AnyVal {
 
     // Always have a maxLength on production, so if you forgot to add one then the default is used.
     // We need to be able to override this behaviour when running integration tests that check that
     // server-side error messages are shown in non-html5 browser
-    def maxLengthRules =
-      (htmlArgs.contains('maxLength), validationOff) match {
-        case (true, true) => htmlArgs - 'maxLength // Remove maxLength when testing so we can have integration tests cause server side validation errors.
-        case (true, false) => htmlArgs // No change
-        case (false, true) => htmlArgs // No change
-        case (false, false) =>
-          // On production we should have a maxLength, so if you forgot to add one then the default is used.
-          htmlArgs + ('maxLength -> DefaultMaxLength)
-      }
+    def withMaxLength = {
+      val DefaultMaxLength = 60
+      if (htmlArgs.contains('maxLength)) htmlArgs // No change
+      else htmlArgs + ('maxLength -> DefaultMaxLength) // On production we should have a maxLength, so if you forgot to add one then the default is used.
+    }
 
     // Always turn off autocomplete to protect user details.
-    def autoCompleteRules: Map[Symbol, Any] =
+    def withoutAutoComplete: Map[Symbol, Any] =
       htmlArgs.get('autocomplete) match {
         case Some(value) =>
           value match {
@@ -36,6 +29,4 @@ object HtmlArgsExtensions {
       }
   }
 
-  implicit def richHtmlArgs(htmlArgs: Map[Symbol, Any]) = new RichHtmlArgs(htmlArgs)
-  // TODO can this be replaced with the way we use implicits in the EncryptedCookieImplicits
 }
--- test/uk/gov/dvla/vehicles/presentation/common/views/helpers/HtmlArgsExtensionsSpec.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/views/helpers/HtmlArgsExtensionsSpec.scala
@@ -5,41 +5,25 @@ import uk.gov.dvla.vehicles.presentation.common.{UnitSpec, WithApplication}
 
 final class HtmlArgsExtensionsSpec extends UnitSpec {
 
-  "HtmlArgsExtensions maxlength rules" should {
-    "remove maxLength from args when key maxLength is present and in test mode" in new WithApplication {
+  "withMaxLength" should {
+    "return the same args when key maxLength is already present" in new WithApplication {
       val richHtmlArgs = new RichHtmlArgs(htmlArgsWithMaxLength)
-
-      val result = richHtmlArgs.maxLengthRules
-
-      result should equal(htmlArgsMinimal)
-    }
-
-    "return the same args when key maxLength is present and in test mode" in new WithApplication {
-      val richHtmlArgs = new RichHtmlArgs(htmlArgsWithMaxLength, validationOff = false)
-
-      val result = richHtmlArgs.maxLengthRules
+      // Override validationOff to check the behaviour of the production code.
+      val result = richHtmlArgs.withMaxLength
 
       result should equal(htmlArgsWithMaxLength)
     }
 
-    "return the same args when key maxLength is not present and test mode" in new WithApplication {
+    "add key maxLength with default value to args not present" in new WithApplication {
       val richHtmlArgs = new RichHtmlArgs(htmlArgsMinimal)
       // Override validationOff to check the behaviour of the production code.
-      val result = richHtmlArgs.maxLengthRules
-
-      result should equal(htmlArgsMinimal)
-    }
-
-    "add key maxLength with default value to args not present and not in test mode" in new WithApplication {
-      val richHtmlArgs = new RichHtmlArgs(htmlArgsMinimal, validationOff = false)
-      // Override validationOff to check the behaviour of the production code.
-      val result = richHtmlArgs.maxLengthRules
+      val result = richHtmlArgs.withMaxLength
 
       result should equal(htmlArgsWithMaxLength)
     }
   }
 
-  "HtmlArgsExtensions autocomplete rules" should {
+  "withoutAutoComplete" should {
     val htmlArgsMinimal: Map[Symbol, Any] = Map('title -> "test")
     val htmlArgsWithAutoCompleteOff: Map[Symbol, Any] = Map('title -> "test", 'autocomplete -> "off")
     val htmlArgsWithAutoCompleteOn: Map[Symbol, Any] = Map('title -> "test", 'autocomplete -> "on")
@@ -47,7 +31,7 @@ final class HtmlArgsExtensionsSpec extends UnitSpec {
     "add autocomplete off when key is not present" in new WithApplication {
       val richHtmlArgs = new RichHtmlArgs(htmlArgsMinimal)
 
-      val result = richHtmlArgs.autoCompleteRules
+      val result = richHtmlArgs.withoutAutoComplete
 
       result should equal(htmlArgsWithAutoCompleteOff)
     }
@@ -55,7 +39,7 @@ final class HtmlArgsExtensionsSpec extends UnitSpec {
     "return the same args when key-value autocomplete 'off' is present" in new WithApplication {
       val richHtmlArgs = new RichHtmlArgs(htmlArgsWithAutoCompleteOff)
 
-      val result = richHtmlArgs.autoCompleteRules
+      val result = richHtmlArgs.withoutAutoComplete
 
       result should equal(htmlArgsWithAutoCompleteOff)
     }
@@ -63,7 +47,7 @@ final class HtmlArgsExtensionsSpec extends UnitSpec {
     "replace key-value autocomplete 'on' with autocomplete 'off'" in new WithApplication {
       val richHtmlArgs = new RichHtmlArgs(htmlArgsWithAutoCompleteOn)
 
-      val result = richHtmlArgs.autoCompleteRules
+      val result = richHtmlArgs.withoutAutoComplete
 
       result should equal(htmlArgsWithAutoCompleteOff)
     }
