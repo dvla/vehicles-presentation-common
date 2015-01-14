32f013c3c5d20cd674157e7876b112ef1b9b8f1e
Chris Rudd <chris.rudd@valtech.co.uk>
2015-01-14 14:00:15 +0000

    US2178: Added disposeFlag and keeperEndDate to VehicleAndKeeperDetailsDto.

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
@@ -1,6 +1,8 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup
 
-import play.api.libs.json.Json
+import org.joda.time.DateTime
+import play.api.data.validation.ValidationError
+import play.api.libs.json._
 
 final case class VehicleAndKeeperDetailsDto(registrationNumber: String,
                                             vehicleMake: Option[String],
@@ -13,9 +15,31 @@ final case class VehicleAndKeeperDetailsDto(registrationNumber: String,
                                             keeperAddressLine3: Option[String],
                                             keeperAddressLine4: Option[String],
                                             keeperPostTown: Option[String],
-                                            keeperPostcode: Option[String])
+                                            keeperPostcode: Option[String],
+                                            disposeFlag: Option[Boolean] = None,
+                                            keeperEndDate: Option[DateTime] = None)
 
 object VehicleAndKeeperDetailsDto {
+
+  implicit val jodaDateReads: Reads[org.joda.time.DateTime] = new Reads[org.joda.time.DateTime] {
+    private val df = org.joda.time.format.ISODateTimeFormat.dateTime()
+
+    private def parseDate(input: String): Option[DateTime] =
+      scala.util.control.Exception.allCatch[DateTime] opt (DateTime.parse(input, df))
+
+    def reads(json: JsValue): JsResult[DateTime] = json match {
+      case JsNumber(d) => JsSuccess(new DateTime(d.toLong))
+      case JsString(s) => parseDate(s) match {
+        case Some(d) => JsSuccess(d)
+        case None => JsError(Seq(JsPath() -> Seq(ValidationError("validate.error.expected.date.isoformat", "ISO8601"))))
+      }
+      case _ => JsError(Seq(JsPath() -> Seq(ValidationError("validate.error.expected.date"))))
+    }
+  }
+
+  implicit val jodaDateWrites: Writes[org.joda.time.DateTime] = new Writes[org.joda.time.DateTime] {
+    def writes(d: org.joda.time.DateTime): JsValue = JsString(d.toString())
+  }
 
   implicit val JsonFormat = Json.format[VehicleAndKeeperDetailsDto]
 }
