de21b1788212ac8fcbc2e2af5d919faf6801f260
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-01-16 11:45:35 +0000

    US2177: Revert some properties to optionals so tests can pass

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/acquire/AcquireConfig.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/acquire/AcquireConfig.scala
@@ -1,10 +1,9 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.acquire
 
-import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.getProperty
+import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.getOptionalProperty
 import scala.concurrent.duration.DurationInt
 
 class AcquireConfig {
-  lazy val baseUrl = getProperty[String]("acquireVehicle.baseUrl")
-  lazy val requestTimeout = getProperty[Int]("acquireVehicle.requestTimeout")
-//  val requestTimeout = getProperty("acquireVehicle.requestTimeout", 5.seconds.toMillis.toInt)
+  lazy val baseUrl = getOptionalProperty[String]("acquireVehicle.baseUrl").getOrElse("")
+  lazy val requestTimeout = getOptionalProperty[Int]("acquireVehicle.requestTimeout").getOrElse(5.seconds.toMillis.toInt)
 }
