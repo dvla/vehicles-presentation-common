f4c78b97e67659bc3c880b1cbe09e2ade8208f33
Chris Rudd <chris.rudd@valtech.co.uk>
2015-01-29 12:30:50 +0000

    Reinstated bruteForecEnable default true.

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/bruteforceprevention/BruteForcePreventionConfig.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/bruteforceprevention/BruteForcePreventionConfig.scala
@@ -13,7 +13,7 @@ class BruteForcePreventionConfig {
 
   lazy val baseUrl: String = getOptionalProperty[String]("bruteForcePrevention.baseUrl").getOrElse("")
   lazy val requestTimeoutMillis: Int = getOptionalProperty[Int]("bruteForcePrevention.requestTimeout").getOrElse(10000)
-  lazy val isEnabled: Boolean = getOptionalProperty[Boolean]("bruteForcePrevention.enabled").getOrElse(false)
+  lazy val isEnabled: Boolean = getOptionalProperty[Boolean]("bruteForcePrevention.enabled").getOrElse(true)
   lazy val nameHeader: String = getOptionalProperty[String]("bruteForcePrevention.headers.serviceName").getOrElse("")
   lazy val maxAttemptsHeader: Int = getOptionalProperty[Int]("bruteForcePrevention.headers.maxAttempts").getOrElse(3)
   lazy val expiryHeader: String = getOptionalProperty[String]("bruteForcePrevention.headers.expiry").getOrElse("")
--- test/uk/gov/dvla/vehicles/presentation/common/webserviceclients/brute_force_prevention/FakeBruteForcePreventionConfig.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/webserviceclients/brute_force_prevention/FakeBruteForcePreventionConfig.scala
@@ -11,7 +11,7 @@ class FakeBruteForcePreventionConfig extends BruteForcePreventionConfig {
 
   override lazy val baseUrl: String = getOptionalProperty[String]("bruteForcePrevention.baseUrl").getOrElse("")
   override lazy val requestTimeoutMillis: Int = getOptionalProperty[Int]("bruteForcePrevention.requestTimeout").getOrElse(5.seconds.toMillis.toInt)
-  override lazy val isEnabled: Boolean = getOptionalProperty[Boolean]("bruteForcePrevention.enabled").getOrElse(false)
+  override lazy val isEnabled: Boolean = getOptionalProperty[Boolean]("bruteForcePrevention.enabled").getOrElse(true)
   override lazy val nameHeader: String = getOptionalProperty[String]("bruteForcePrevention.headers.serviceName").getOrElse("")
   override lazy val maxAttemptsHeader: Int = getOptionalProperty[Int]("bruteForcePrevention.headers.maxAttempts").getOrElse(3)
   override lazy val expiryHeader: String = getOptionalProperty[String]("bruteForcePrevention.headers.expiry").getOrElse("")
