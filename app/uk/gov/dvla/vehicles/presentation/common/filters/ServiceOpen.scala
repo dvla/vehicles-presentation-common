d6f9f484b91f990219e250124952be51a660433b
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-03-13 10:00:48 +0000

    Add the version to the our of hours exceptions

--- app/uk/gov/dvla/vehicles/presentation/common/filters/ServiceOpen.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/filters/ServiceOpen.scala
@@ -1,9 +1,12 @@
 package uk.gov.dvla.vehicles.presentation.common.filters
 
+import uk.gov.dvla.vehicles.presentation.common.controllers.Version
+
 object ServiceOpen {
   val whitelist = List (
     "/assets/",
     "/help",
-    "/feedback"
+    "/feedback",
+    Version.Suffix
   )
 }
