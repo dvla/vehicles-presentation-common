54b6b8384739c657fed229dc2a651b354749b95c
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-03-17 12:14:00 +0000

    RELEASE-2.18

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "2.18-SNAPSHOT"
+  val versionString = "2.18"
   val scalaVersionString = "2.10.4"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
