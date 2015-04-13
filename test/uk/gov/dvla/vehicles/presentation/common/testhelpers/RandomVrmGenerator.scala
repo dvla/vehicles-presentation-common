98355c17233451d08749dc40c2e4b16fbd469746
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-04-13 16:03:20 +0100

    Add a uniqueVrm

--- test/uk/gov/dvla/vehicles/presentation/common/testhelpers/RandomVrmGenerator.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/testhelpers/RandomVrmGenerator.scala
@@ -1,20 +1,33 @@
 package uk.gov.dvla.vehicles.presentation.common.testhelpers
 
 import scala.collection.immutable.NumericRange.Inclusive
+import scala.collection.mutable
+import scala.collection.parallel.mutable
 import scala.util.Random
 
 object RandomVrmGenerator {
   private final val letters: Inclusive[Char] = 'A' to 'Z'
   private final val numbers: Inclusive[Char] = '0' to '9'
+  private final val usedVrms = new scala.collection.mutable.HashSet[String]()
 
   def vrm = {
     // Create random reg in this format: YL07YBX
     s"${randomString(letters, 2)}${randomString(numbers, 2)}${randomString(letters, 3)}"
   }
 
+  def docRef = randomString(numbers,11)
+
   def randomString(alphabet: Inclusive[Char], n: Int): String =
     Stream.continually(Random.nextInt(alphabet.size)).
       map(letter => alphabet(letter)).
       take(n).
       mkString
+
+
+  def uniqueVrm: String = vrm match {
+    case x if vrm.contains(x) => uniqueVrm
+    case x =>
+      usedVrms.add(x)
+      x
+  }
 }
