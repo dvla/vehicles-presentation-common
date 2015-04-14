9c5923e1f9636a45858d8bf626748e7e051d7119
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-04-14 10:09:39 +0100

    DE418 add debug information during date conversion in the webapp

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperDetailsDto.scala
@@ -1,6 +1,7 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup
 
 import org.joda.time.{DateTimeZone, DateTime}
+import play.Logger
 import play.api.data.validation.ValidationError
 import play.api.libs.json.{JsNumber, JsResult, JsValue, Reads, JsSuccess, JsError, JsString, JsPath, Writes, Json}
 
@@ -27,13 +28,20 @@ object VehicleAndKeeperDetailsDto {
   implicit val jodaDateReads: Reads[org.joda.time.DateTime] = new Reads[org.joda.time.DateTime] {
     private val df = org.joda.time.format.ISODateTimeFormat.dateTime().withZone(dtz)
 
-    private def parseDate(input: String): Option[DateTime] =
+    private def parseDate(input: String): Option[DateTime] = {
+      Logger.info(s"DE418 parsing date $input into ${DateTime.parse(input, df)}")
       scala.util.control.Exception.allCatch[DateTime] opt (DateTime.parse(input, df))
+    }
 
     def reads(json: JsValue): JsResult[DateTime] = json match {
-      case JsNumber(d) => JsSuccess(new DateTime(d.toLong, dtz))
+      case JsNumber(d) =>
+        val result = new DateTime(d.toLong, dtz)
+        Logger.info(s"DE418 converting a JsNumber($d) is converted to $result")
+        JsSuccess(result)
       case JsString(s) => parseDate(s) match {
-        case Some(d) => JsSuccess(d)
+        case Some(d) =>
+          Logger.info(s"DE418 converting a JsString($d) is converted to $d")
+          JsSuccess(d)
         case None => JsError(Seq(JsPath() -> Seq(ValidationError("validate.error.expected.date.isoformat", "ISO8601"))))
       }
       case _ => JsError(Seq(JsPath() -> Seq(ValidationError("validate.error.expected.date"))))
