9ed1b761fb68862e199f03d2cb0d399076936d76
Ian Stainer <ian.stainer@valtech.co.uk>
2015-04-16 17:40:27 +0100

    US2889 javascript changes around making the functions in global-helpers global and other tidy up

--- app/assets/javascripts/autofill-todays-date.js
+++ app/assets/javascripts/autofill-todays-date.js
@@ -4,8 +4,8 @@ function AutoFillTodaysDate(day, month, year, checkboxId, dayId, monthId, yearId
     var dod_month = document.getElementById(monthId);
     var dod_year = document.getElementById(yearId);
 
-    var padded_day = ('0' + day).substr(('0' + day).length - 2, 2)
-    var padded_month = ('0' + month).substr(('0' + month).length - 2, 2)
+    var padded_day = ('0' + day).substr(('0' + day).length - 2, 2);
+    var padded_month = ('0' + month).substr(('0' + month).length - 2, 2);
 
     if (dod_elm.checked === false) {
         dod_day.value = '';
