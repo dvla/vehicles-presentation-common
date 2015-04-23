a0162d3f426fe17491224b4d4d985e3c00857485
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-04-23 14:06:21 +0100

    US2860 add a formatter for the DatePicker widget

---
+++ test/uk/gov/dvla/vehicles/presentation/common/mappings/AddressPickerSpec.scala
@@ -0,0 +1,110 @@
+package uk.gov.dvla.vehicles.presentation.common.mappings
+
+import play.api.data.FormError
+import uk.gov.dvla.vehicles.presentation.common.UnitSpec
+import uk.gov.dvla.vehicles.presentation.common.model.AddressPickerModel
+import AddressPicker.{AddressLine1Id, AddressLine2Id,AddressLine3Id, PostTownId, PostcodeId, CountyId}
+import uk.gov.dvla.vehicles.presentation.common.views.constraints
+
+class AddressPickerSpec extends UnitSpec {
+  private val formatter = AddressPicker.formatter()
+  val fullModel = AddressPickerModel(
+    "address line 1",
+    Some("address line 2"),
+    Some("address line 3"),
+    "Post town",
+    Some("Orange county"),
+    "N19 3NN"
+  )
+  val missingOptional = fullModel.copy(streetAddress2 = None, streetAddress3 = None, county = None)
+
+
+  "formatter unbing" should {
+    "generate the right data from all fields" in {
+      formatter.unbind("datePicker1", fullModel) should equal(Map(
+        s"datePicker1.$AddressLine1Id" -> fullModel.streetAddress1,
+        s"datePicker1.$AddressLine2Id" -> fullModel.streetAddress2.get,
+        s"datePicker1.$AddressLine3Id" -> fullModel.streetAddress3.get,
+        s"datePicker1.$PostTownId" -> fullModel.postTown,
+        s"datePicker1.$CountyId" -> fullModel.county.get,
+        s"datePicker1.$PostcodeId" -> fullModel.postCode
+      ))
+    }
+
+    "generate the right data from mandatory fields only" in {
+      formatter.unbind("datePicker5", missingOptional) should equal(Map(
+        s"datePicker5.$AddressLine1Id" -> fullModel.streetAddress1,
+        s"datePicker5.$PostTownId" -> fullModel.postTown,
+        s"datePicker5.$PostcodeId" -> fullModel.postCode
+      ))
+    }
+  }
+
+  "formatter bind" should {
+    "bind valid data" in {
+      formatter.bind("datePicker1", Map(
+        s"datePicker1.$AddressLine1Id" -> fullModel.streetAddress1,
+        s"datePicker1.$AddressLine2Id" -> fullModel.streetAddress2.get,
+        s"datePicker1.$AddressLine3Id" -> fullModel.streetAddress3.get,
+        s"datePicker1.$PostTownId" -> fullModel.postTown,
+        s"datePicker1.$CountyId" -> fullModel.county.get,
+        s"datePicker1.$PostcodeId" -> fullModel.postCode
+      )) should equal (Right(fullModel))
+
+      formatter.bind("datePicker2", formatter.unbind("datePicker2", fullModel)) should equal(Right(fullModel))
+    }
+
+    "bind valid data with optional fields" in {
+      formatter.bind("dp2", formatter.unbind("dp2", missingOptional)) should equal(Right(missingOptional))
+    }
+
+    "validate required address line 1" in {
+      formatter.bind("dp", formatter.unbind("dp", fullModel.copy(streetAddress1 = ""))) should equal(
+        Left(Seq(FormError(s"dp.$AddressLine1Id", "error.address.addressLine1")))
+      )
+    }
+
+    "validate required post town" in {
+      formatter.bind("dp", formatter.unbind("dp", fullModel.copy(postTown = ""))) should equal(
+        Left(Seq(FormError(s"dp.$PostTownId", "error.address.postTown")))
+      )
+    }
+
+    "validate required post code" in {
+      formatter.bind("dp", formatter.unbind("dp", fullModel.copy(postCode = ""))) should equal(
+        Left(Seq(FormError(s"dp.$PostcodeId", "error.address.postCode")))
+      )
+    }
+
+    "catch illegal characters in address lines and post town" in {
+      val illegalChars = AddressPickerModel(
+        "address line *&^*&^%$^ 1",
+        Some("@#^&*$address line 2"),
+        Some("address line 3 |||(*&(*"),
+        "Post town(&(*&(",
+        Some("Orange county"),
+        "1@#*^"
+      )
+      formatter.bind("dp", formatter.unbind("dp", illegalChars)) should equal(
+        Left(Seq(
+          FormError(s"dp.$AddressLine1Id", "error.address.characterInvalid"),
+          FormError(s"dp.$AddressLine2Id", "error.address.characterInvalid"),
+          FormError(s"dp.$AddressLine3Id", "error.address.characterInvalid"),
+          FormError(s"dp.$PostTownId", "error.postTown.characterInvalid"),
+          FormError(s"dp.$PostcodeId", "error.restricted.validPostcode", Seq(constraints.Postcode.regex))
+        ))
+      )
+    }
+
+    "validate min length of the post code" in {
+      formatter.bind("dp", formatter.unbind("dp", fullModel.copy(postCode = "AAA "))) match {
+        case Left(errors) => errors should contain(FormError(s"dp.$PostcodeId", "error.minLength", Seq(5)))
+        case _ => fail("Errors expected at that point")
+      }
+    }
+
+    "validate max length of the fields" in {
+
+    }
+  }
+}
