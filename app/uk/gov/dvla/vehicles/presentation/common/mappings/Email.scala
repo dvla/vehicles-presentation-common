aa82e2d68cc5586d75eb7151b03bd11fbff7d2a6
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-04-28 13:01:15 +0100

    US3045 add a new mapping for the confirming email

--- app/uk/gov/dvla/vehicles/presentation/common/mappings/Email.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/mappings/Email.scala
@@ -1,11 +1,16 @@
 package uk.gov.dvla.vehicles.presentation.common.mappings
 
+import org.joda.time.LocalDate
 import play.api.data.format.Formats.stringFormat
 import play.api.data.Forms.of
-import play.api.data.Mapping
+import play.api.data.{FormError, Mapping}
+import play.api.data.format.Formatter
 import uk.gov.dvla.vehicles.presentation.common.views.constraints.Email.emailAddress
 
 object Email {
+  final val EmailId = "email"
+  final val EmailVerifyId = "email-verify"
+
   final val EmailMinLength = 3
   final val EmailMaxLength = 254
   final val EmailUsernameMaxLength = 64
@@ -14,5 +19,21 @@ object Email {
   final val InvalidDomainStartEndChar = "-"
   final val InvalidDomainContentChar = "/"
 
+  def formatter() = new Formatter[String] {
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] =
+      (data.get(s"$key.${Email.EmailId}"), data.get(s"$key.${Email.EmailVerifyId}")) match {
+        case (None, None) => Left(Seq(FormError(key, "error.email")))
+        case (Some(email), Some(emailConfirm)) if email == emailConfirm => Right(email)
+        case _ =>  Left(Seq(FormError(key, "error.email.not.match")))
+      }
+
+    override def unbind(key: String, value: String): Map[String, String] = Map(
+      Email.EmailId -> value,
+      Email.EmailVerifyId -> value
+    )
+  }
+
   def email: Mapping[String] = of[String] verifying emailAddress
+
+  def email1: Mapping[String] = of[String](formatter()) verifying emailAddress
 }
---
+++ test/uk/gov/dvla/vehicles/presentation/common/mappings/EmailSpec.scala
@@ -0,0 +1,48 @@
+package uk.gov.dvla.vehicles.presentation.common.mappings
+
+import play.api.data.FormError
+import uk.gov.dvla.vehicles.presentation.common.UnitSpec
+
+class EmailSpec extends UnitSpec {
+  private val formatter = Email.formatter()
+
+  "bind" should {
+    "pass a emails with the same value" in {
+      formatter.bind("email1", Map(
+        s"email1.${Email.EmailId}" -> "abc@xyz.com",
+        s"email1.${Email.EmailVerifyId}" -> "abc@xyz.com"
+      )) should equal(Right("abc@xyz.com"))
+    }
+
+    "generate error for emails with different values" in {
+      formatter.bind("email1", Map(
+        s"email1.${Email.EmailId}" -> "abc@xyz.com",
+        s"email1.${Email.EmailVerifyId}" -> "xyz@abc.com"
+      )) should equal(Left(Seq(FormError(s"email1", "error.email.not.match"))))
+    }
+
+    "generate error for missing email field" in {
+      formatter.bind("email1", Map(
+        s"email1.${Email.EmailVerifyId}" -> "xyz@abc.com"
+      )) should equal(Left(Seq(FormError(s"email1", "error.email.not.match"))))
+    }
+
+    "generate error for missing email confirm field" in {
+      formatter.bind("email1", Map(
+        s"email1.${Email.EmailId}" -> "xyz@abc.com"
+      )) should equal(Left(Seq(FormError(s"email1", "error.email.not.match"))))
+    }
+
+    "generate error for missing both email fields" in {
+      formatter.bind("email1", Map(
+      )) should equal(Left(Seq(FormError(s"email1", "error.email"))))
+    }
+  }
+
+  "unbind should populate both fields" in {
+    formatter.unbind("email1", "abc@xyz.com") should equal(Map(
+      Email.EmailId -> "abc@xyz.com",
+      Email.EmailVerifyId -> "abc@xyz.com"
+    ))
+  }
+}
