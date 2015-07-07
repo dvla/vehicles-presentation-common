2678380e34ad068f246397998518f3205b917b3b
Chris Rudd <chris.rudd@valtech.co.uk>
2015-07-07 07:33:02 +0100

    US565: Reject email addresses containing spaces.

--- app/uk/gov/dvla/vehicles/presentation/common/views/constraints/Email.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/views/constraints/Email.scala
@@ -82,7 +82,7 @@ object Email {
   def emailAddress: Constraint[String] = Constraint[String]("constraint.email") {
     e =>
       if (!(EmailMinLength to EmailMaxLength contains e.length)) Invalid(ValidationError("error.email"))
-      else if (ptr.matcher(e).matches())
+      else if (ptr.matcher(e).matches() && !e.contains(" "))
         if (emailStyleValid(e)) Valid else Invalid(ValidationError("error.email"))
       else Invalid(ValidationError("error.email"))
   }
--- test/uk/gov/dvla/vehicles/presentation/common/views/constraints/EmailUnitSpec.scala
+++ test/uk/gov/dvla/vehicles/presentation/common/views/constraints/EmailUnitSpec.scala
@@ -32,7 +32,8 @@ final class EmailUnitSpec extends UnitSpec {
       "a@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghikl.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghikl.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghikl.abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg.hijk",
       "\"\"\"@iana.org", "test\"@iana.org", "\"test@iana.org", "\"test\"test@iana.org", "test\"text\"@iana.org",
       "\"test\"\"test\"@iana.org", "\"test\".\"test\"@iana.org", "\"test\".test@iana.org", "test@iana.org-", "(test@iana.org",
-      "test@(iana.org", "\"test\\\"@iana.org", "test@.org", "test@iana/icann.org")
+      "test@(iana.org", "\"test\\\"@iana.org", "test@.org", "test@iana/icann.org", "test @iana.org", "test@ iana.org",
+      "test@iana .org")
 
     invalidEmails.foreach { email =>
       s"indicate the email is invalid: $email" in {
