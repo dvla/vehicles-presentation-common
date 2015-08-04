96210e445374a3075d24742d723d27109f07860a
Oliver Jenkins <oliver.jenkins@dvla.gsi.gov.uk>
2015-08-04 09:07:04 +0100

    US3664 Improved logging methods used for project

--- app/uk/gov/dvla/vehicles/presentation/common/LogFormats.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/LogFormats.scala
@@ -1,11 +1,15 @@
 package uk.gov.dvla.vehicles.presentation.common
 
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.TrackingId
+import play.api.{LoggerLike, Logger}
+import play.api.mvc.Request
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichCookies
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 
 object LogFormats {
 
   private final val anonymousChar = "*"
-  private final val logSeperator = " - "
+  private final val logSeperator = "\t"
   private final val nullString = "null"
   final val optionNone = "none"
 
@@ -30,13 +34,37 @@ object LogFormats {
     }
   }
 
+
+  trait DVLALogger {
+
+    implicit val logger: org.slf4j.Logger = Logger.logger
+
+    sealed trait LogMessageType
+    case object Debug extends LogMessageType
+    case object Info extends LogMessageType
+    case object Error extends LogMessageType
+    case object Warn extends LogMessageType
+
+    def logMessage(trackingId: TrackingId, messageType: LogMessageType, messageText: String, logData: Option[Seq[String]] = None)
+                  (implicit logger: org.slf4j.Logger) =
+      messageType match {
+        case Debug => logger.debug(logMessageFormat(trackingId, messageText, logData))
+        case Info => logger.info(logMessageFormat(trackingId, messageText, logData))
+        case Error => logger.error(logMessageFormat(trackingId, messageText, logData))
+        case Warn => logger.warn(logMessageFormat(trackingId, messageText, logData))
+      }
+
+
+    private def logMessageFormat(trackingId: TrackingId, messageText: String, logData: Option[Seq[String]]): String =
+      s"""[TrackingID: ${trackingId.value}]$logSeperator$messageText ${logData.map( d => s"$logSeperator$logData" ).getOrElse("")}"""
+  }
   // When changes these two logMessage methods, be sure to replicate the changes in
   //    vehicles-services-common/src/main/scala/dvla.common/LogFormats.scala
   // in order to keep a consistent log format
-  def logMessage(messageText: String, trackingId: TrackingId, logData: Seq[String]): String =
-    messageText + logSeperator + logData + "trackingId: " + trackingId.value
-
-  def logMessage(messageText: String, trackingId: TrackingId): String =
-    messageText + logSeperator + "trackingId: " + trackingId.value
+//  def logMessage(messageText: String, trackingId: TrackingId, logData: Seq[String]): String =
+//    messageText + logSeperator + logData + "trackingId: " + trackingId.value
+//
+//  def logMessage(messageText: String, trackingId: TrackingId): String =
+//    messageText + logSeperator + "trackingId: " + trackingId.value
 
 }
