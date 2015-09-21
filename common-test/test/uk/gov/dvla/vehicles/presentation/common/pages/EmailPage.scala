096b02f0dc74255c4e5e611cd5b1204a15c91f43
Oliver Jenkins <oliver.jenkins@dvla.gsi.gov.uk>
2015-09-21 14:44:52 +0100

    US3815 fix for breaking tests

--- common-test/test/uk/gov/dvla/vehicles/presentation/common/pages/EmailPage.scala
+++ common-test/test/uk/gov/dvla/vehicles/presentation/common/pages/EmailPage.scala
@@ -1,11 +1,7 @@
 package uk.gov.dvla.vehicles.presentation.common.pages
 
 import org.openqa.selenium.WebDriver
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.Element
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.Page
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.TextField
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.WebBrowserDSL
-import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.WebDriverFactory
+import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser._
 import uk.gov.dvla.vehicles.presentation.common.mappings.Email.{EmailId => EmailEnterId, EmailVerifyId}
 import uk.gov.dvla.vehicles.presentation.common.models.EmailModel.Form.EmailId
 
@@ -16,9 +12,9 @@ object EmailPage extends Page with WebBrowserDSL {
   final override val title: String = "Email capture"
   val emailValid = "test@test.com"
 
-  def businessEmail(implicit driver: WebDriver): TextField = textField(id(s"${EmailId}_$EmailEnterId"))
+  def businessEmail(implicit driver: WebDriver): EmailField = emailField(id(s"${EmailId}_$EmailEnterId"))
 
-  def businessEmailVerify(implicit driver: WebDriver): TextField = textField(id(s"${EmailId}_$EmailVerifyId"))
+  def businessEmailVerify(implicit driver: WebDriver): EmailField = emailField(id(s"${EmailId}_$EmailVerifyId"))
 
   def submit(implicit driver: WebDriver): Element = find(id("submit")).get
 
