5a8cb535615a5de8479daa04eea7686fc65b2914
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2015-11-13 16:12:30 +0000

    US3956 employ widget for (radio) input

--- app/uk/gov/dvla/vehicles/presentation/common/views/models/RadioWithHint.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/views/models/RadioWithHint.scala
@@ -1,3 +1,3 @@
 package uk.gov.dvla.vehicles.presentation.common.views.models
 
-case class RadioWithHint(key: String, message: String, hint: String)
+case class RadioWithHint(key: String, message: String, hint: String, helpHtml: String = "")
