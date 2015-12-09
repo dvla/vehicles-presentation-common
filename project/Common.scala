0b204fb4f318b52113f9651dd5b05720fc083910
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-12-09 09:18:11 +0000

    New development version 2.39-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "2.38"
+  val versionString = "2.39-SNAPSHOT"
   val scalaVersionString = "2.10.5"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
