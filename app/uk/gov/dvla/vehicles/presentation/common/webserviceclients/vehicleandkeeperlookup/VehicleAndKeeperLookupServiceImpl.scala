78f28f8b4e4ebbe56944889eda164a556f6394a7
Ian Stainer <ian.stainer@valtech.co.uk>
2016-02-05 13:06:52 +0000

    US4216 replaced INTERNAL_SERVER_ERROR 500 with NOT_FOUND 404 when dealing with a failed vehicle lookup

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperLookupServiceImpl.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/vehicleandkeeperlookup/VehicleAndKeeperLookupServiceImpl.scala
@@ -21,19 +21,29 @@ final class VehicleAndKeeperLookupServiceImpl @Inject()(ws: VehicleAndKeeperLook
       if (resp.status == Status.OK) {
         healthStats.success(ServiceName)
         Right(resp.json.as[VehicleAndKeeperLookupSuccessResponse])
-      } else if (resp.status == Status.INTERNAL_SERVER_ERROR) {
-        val e =  new RuntimeException(
-          s"Vehicle and keeper lookup web service call failed with ${resp.status}. " +
-          s"body: ${resp.body}'. Problem may come from either vehicle and keeper " +
-          s"lookup micro-service or the VPDS - trackingId: $trackingId"
-        )
-        healthStats.failure(ServiceName, e)
-        Left(resp.json.as[VehicleAndKeeperLookupFailureResponse])
+      } else if (resp.status == Status.NOT_FOUND) {
+        val msg = "Vehicle and keeper lookup web service failed to lookup vehicle. " +
+          s"Returned with status: ${resp.status} body: ${parseResponseBody(resp.body)}"
+        logMessage(trackingId, Debug, msg)
+        if (resp.body.startsWith("The requested resource could not be found")) {
+          // The 404 indicates that there is no resource listening on the url that was specified
+          // so we report this as a failure to the healthStats service
+          val e = new RuntimeException(
+            "Vehicle and keeper lookup web service call http status not OK, it " +
+              s"was status: ${resp.status} body: ${resp.body}"
+          )
+          healthStats.failure(ServiceName, e)
+          throw e
+        } else {
+          // The 404 indicates a business failure so we report that as a success to the healthStats service
+          healthStats.success(ServiceName)
+          Left(resp.json.as[VehicleAndKeeperLookupFailureResponse])
+        }
       } else {
         val e =  new RuntimeException(
-          s"Vehicle and keeper lookup web service call http status not OK, it " +
-          s"was: '${resp.status} body: ${resp.body}'. Problem may come from either vehicle and keeper " +
-          s"lookup micro-service or the VPDS - trackingId: $trackingId"
+          "Vehicle and keeper lookup web service call http status not OK, it " +
+          s"was status: ${resp.status} body: ${resp.body}. Problem may come from either vehicle and keeper " +
+          s"lookup micro-service or VPDS"
         )
         healthStats.failure(ServiceName, e)
         throw e
@@ -43,6 +53,13 @@ final class VehicleAndKeeperLookupServiceImpl @Inject()(ws: VehicleAndKeeperLook
         healthStats.failure(ServiceName, e)
         throw e
     }
+
+  private def parseResponseBody(responseBody: String): String = {
+    def isJsonResponse = responseBody.startsWith("{")
+
+    val whiteSpaceAndCarriageReturnRegex = "[\\s\\r\\n]"
+    if (isJsonResponse) responseBody.replaceAll(whiteSpaceAndCarriageReturnRegex, "") else responseBody
+  }
 }
 
 object VehicleAndKeeperLookupServiceImpl {
