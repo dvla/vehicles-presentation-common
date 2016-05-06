796aaaafa7948d0f9b85a5b037c4d6bf918d8924
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-05-06 10:58:45 +0100

    VMPRCI-4351 Add messages file test to identify missing values

---
+++ test/uk/gov/dvla/vehicles/presentation/common/conf/MessageFilesSpec.scala
@@ -0,0 +1,36 @@
+package uk.gov.dvla.vehicles.presentation.common.conf
+
+import uk.gov.dvla.vehicles.presentation.common.UnitSpec
+import uk.gov.dvla.vehicles.presentation.common.testhelpers.MessageFilesSpecHelper.messagesFilesHelper
+import uk.gov.dvla.vehicles.presentation.common.testhelpers.MessageFilesSpecHelper.ENGLISH_FILE
+import uk.gov.dvla.vehicles.presentation.common.testhelpers.MessageFilesSpecHelper.WELSH_FILE
+
+class MessageFilesSpec extends UnitSpec {
+
+  val englishKeys = messagesFilesHelper.extractMessageKeys(ENGLISH_FILE)
+  val welshKeys = messagesFilesHelper.extractMessageKeys(WELSH_FILE)
+  val mapEnglish = messagesFilesHelper.extractMessageMap(ENGLISH_FILE)
+  val mapWelsh = messagesFilesHelper.extractMessageMap(WELSH_FILE)
+
+  "Message files" should {
+    "have a corresponding Welsh key for each English key" in {
+      val englishKeysWithNoWelshEquivalent = englishKeys.filterNot(enKey => welshKeys.contains(enKey))
+      println(s"English keys that are missing in the Welsh message file: $englishKeysWithNoWelshEquivalent")
+      englishKeysWithNoWelshEquivalent should equal(List.empty)
+    }
+
+    "have a corresponding English key for each Welsh key" in {
+      val welshKeysWithNoEnglishEquivalent = welshKeys.filterNot(cyKey => englishKeys.contains(cyKey))
+      println(s"Welsh keys that are missing in the English message file: $welshKeysWithNoEnglishEquivalent")
+      welshKeysWithNoEnglishEquivalent should equal(List.empty)
+    }
+
+    "have an English value and a corresponding non-blank Welsh value" in {
+      messagesFilesHelper.getBlankNonBlankValuesCount(mapEnglish, mapWelsh) should equal(0)
+    }
+
+    "have a Welsh value and a corresponding non-blank English value" in {
+      messagesFilesHelper.getBlankNonBlankValuesCount(mapWelsh, mapEnglish) should equal(0)
+    }
+  }
+}
