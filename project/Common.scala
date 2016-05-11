e219a7724fd2587fb361864aaa5e8de9af9305c2
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-05-11 11:42:36 +0100

    New development version 2.49-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt.Keys.version
 import sbt._
 
 object Common {
-  val versionString = "2.48"
+  val versionString = "2.49-SNAPSHOT"
   val scalaVersionString = "2.10.5"
   val organisationString = "dvla"
   val organisationNameString = "Driver & Vehicle Licensing Agency"
