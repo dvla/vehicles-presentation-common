e5bc02155d5cf8b922390d2d20d211b3368c71b8
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-05-13 12:39:11 +0100

    VMPRCI-4359 Revert to original VMPR feedback questionnaire

--- app/uk/gov/dvla/vehicles/presentation/common/model/FeedbackForm.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/model/FeedbackForm.scala
@@ -6,45 +6,22 @@ import play.api.data.format.Formatter
 import uk.gov.dvla.vehicles.presentation.common.mappings.Email.email
 import play.api.libs.json.Json
 import play.api.data.Forms.of
-import uk.gov.dvla.vehicles.presentation.common.mappings.OptionalToggleOptional
 
-case class FeedbackForm(feedback: String, webChat: Option[String], name: Option[String], email: Option[String])
+case class FeedbackForm(feedback: String, name: Option[String], email: Option[String])
 
 object FeedbackForm {
 
   object Form {
     final val feedback = "feedback"
-    final val webChatOption = "feedbackWebChatOption"
-    final val webChat = "feedbackWebChat"
     final val nameMapping = "feedbackName"
     final val emailMapping = "feedbackEmail"
 
     final val Mapping = mapping(
       feedback -> nonEmptyText(minLength = 2, maxLength = 500),
-      webChatOption -> OptionalToggleOptional.optional(WebChatFeedback.optional.withPrefix(webChat)),
       nameMapping -> optional(text(minLength = 2, maxLength = 60)),
       emailMapping -> optional(email)
     )(FeedbackForm.apply)(FeedbackForm.unapply)
   }
 
   implicit val JsonFormat = Json.format[FeedbackForm]
-}
-
-object WebChatFeedback {
-
-  def formatter() = new Formatter[String] {
-    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], String] = {
-      (data.get(FeedbackForm.Form.webChatOption), data.get(key)) match {
-        case (None, _) => Right("Webchat was not offered")
-        case (_, Some(wc)) if wc.length >= 2 && wc.length <= 500 => Right(wc)
-        case _ => Left(Seq(FormError(key, "error.feedback")))
-      }
-    }
-
-    override def unbind(key: String, value: String): Map[String, String] = Map(
-      FeedbackForm.Form.webChat -> value
-    )
-  }
-
-  def optional: Mapping[String] = of[String](formatter())
 }
