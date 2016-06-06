c1aa67c0554c9e7fac354690c4696c7a22131b95
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-06-06 16:19:46 +0100

    VMPRCI-4215 Remove qunit

--- common-test/app/uk/gov/dvla/vehicles/presentation/common/controllers/OptionToggleController.scala
+++ common-test/app/uk/gov/dvla/vehicles/presentation/common/controllers/OptionToggleController.scala
@@ -13,10 +13,6 @@ class OptionToggleController @Inject()(implicit clientSideSessionFactory: Client
     OptionalToggleModel.Form.Mapping
   )
 
-  def jsTest = Action { implicit request =>
-    Ok(views.html.optionToggle(form, true))
-  }
-
   def present = Action { implicit request =>
     Ok(views.html.optionToggle(form))
   }
