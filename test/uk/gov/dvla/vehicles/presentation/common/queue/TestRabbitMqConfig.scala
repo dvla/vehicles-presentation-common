e54b6777927a3f3e5fcbb7f0d5333277aa20d6d0
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-08-02 10:29:25 +0100

    VMPRCI-4476 Remove unused RabbitMQ API

--- test/uk/gov/dvla/vehicles/presentation/common/queue/TestRabbitMqConfig.scala
+++
@@ -1,9 +0,0 @@
-package uk.gov.dvla.vehicles.presentation.common.queue
-
-case class TestRabbitMqConfig(rabbitMqVirtualHost: Option[String],
-                              rabbitMqUserName: Option[String],
-                              rabbitMqPassword: Option[String],
-                              rabbitMqUseSsl: Boolean,
-                              rabbitMqNetworkRecoverIntervalMs: Option[Long],
-                              rabbitMqHeartBeat: Option[Int],
-                              rabbitMqAddresses: Array[com.rabbitmq.client.Address]) extends RabbitMqConfig
