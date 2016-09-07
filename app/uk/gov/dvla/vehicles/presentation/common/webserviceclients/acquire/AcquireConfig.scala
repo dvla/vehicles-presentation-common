d839d54c90fe9d5fc06da701d68d29abb55ee1e2
Ian Stainer <ian.stainer@valtech.co.uk>
2016-09-07 12:03:56 +0100

    VMPRCI-4513 changes to make acquire vehicle and email service base urls mandatory

--- app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/acquire/AcquireConfig.scala
+++ app/uk/gov/dvla/vehicles/presentation/common/webserviceclients/acquire/AcquireConfig.scala
@@ -1,10 +1,11 @@
 package uk.gov.dvla.vehicles.presentation.common.webserviceclients.acquire
 
 import scala.concurrent.duration.DurationInt
-import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.{getOptionalProperty, intProp, stringProp}
+import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.{getOptionalProperty, getProperty, intProp, stringProp}
 import uk.gov.dvla.vehicles.presentation.common.utils.helpers.CommonConfig
 
 class AcquireConfig {
-  lazy val baseUrl = getOptionalProperty[String]("acquireVehicle.baseUrl").getOrElse(CommonConfig.DEFAULT_BASE_URL)
-  lazy val requestTimeout = getOptionalProperty[Int]("acquireVehicle.requestTimeout").getOrElse(CommonConfig.DEFAULT_REQ_TIMEOUT.seconds.toMillis.toInt)
+  lazy val baseUrl = getProperty[String]("acquireVehicle.baseUrl")
+  lazy val requestTimeout = getOptionalProperty[Int]("acquireVehicle.requestTimeout")
+    .getOrElse(CommonConfig.DEFAULT_REQ_TIMEOUT.seconds.toMillis.toInt)
 }
